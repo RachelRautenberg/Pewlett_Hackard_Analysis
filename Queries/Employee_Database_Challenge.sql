54b581a403b945b8ae6d50d06f537f5519c4a7c0
RachelRautenberg <96299861+RachelRautenberg@users.noreply.github.com>
2022-02-11 11:51:33 -0600

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -30,3 +30,21 @@ SELECT title,
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
+
+-- Mentorship Eligibility
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no ASC
